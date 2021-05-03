3738166fbd35d01cfc354c2b3769645b775da87b
Noah Albrecht <Noahschbern@hotmail.com>
2021-05-03 20:27:05 +0200

    Function (+) Calculator

--- src/com/example/mainfolder/Main.java
+++ src/com/example/mainfolder/Main.java
@@ -1,7 +1,16 @@
 package com.example.mainfolder;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Enter first Number:");
+        Scanner userinput = new Scanner(System.in);
+        Integer firstnum = Integer.parseInt(userinput.nextLine());
+        System.out.println("Enter second Number:");
+        Integer secondnum = Integer.parseInt(userinput.nextLine());
+
+        Integer result = firstnum + secondnum;
+        System.out.println("Dein Resultat ist:" + result);
+
     }
 }
